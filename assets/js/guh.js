58c957230f67307b97a94e12b6c16e1876012858
Sami Alabed <sami.alabed@student.manchester.ac.uk>
2015-09-09 17:26:27 +0100

    Material design compressored

---
+++ assets/js/guh.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Sami Alabed on 09/09/15.
+ */
+
+(function () {
+  function initialize() {
+    var mapCanvas = document.getElementById('map-canvas');
+    var locationLatLon = new google.maps.LatLng(53.479251, -2.247926); //todo change to venue latln
+    var mapOptions = {
+      center: locationLatLon,
+      zoom: 10, //18 when confirm venue
+      mapTypeControl: true,
+      scrollwheel: false,
+      navigationControl: false,
+      streetViewControl: false,
+      disableDefaultUI: true,
+      styles: [
+        {"stylers": [{"hue": "#FFCC33"}]},
+        {"featureType": "road", "elementType": "labels", "stylers": [{"visibility": "on"}]},
+        {
+          "featureType": "road", "elementType": "geometry", "stylers": [{"lightness": 100},
+          {"visibility": "simplified"}]
+        }]
+    };
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+    var marker = new google.maps.Marker({
+      position: locationLatLon,
+      map: map,
+      title: 'GreatUniHack 2016'
+    });
+  }
+
+  google.maps.event.addDomListener(window, 'load', initialize);
+})();
